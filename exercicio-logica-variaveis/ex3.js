a5ccad22bc0eaacd37bd0a7d5fd7fd2012eb0e26
Lorena Paixão <lorenamagalhaes4127@gmail.com>
2023-07-24 12:45:09 -0300

    exercicio3

---
+++ exercicio-logica-variaveis/ex3.js
@@ -0,0 +1,6 @@
+const meuDinheiro = 80;
+const precoTenis = 129.99;
+const descontoEmReais = precoTenis - meuDinheiro;
+const percentualDesconto = (descontoEmReais * 100) / 129.99;
+
+console.log(percentualDesconto);
