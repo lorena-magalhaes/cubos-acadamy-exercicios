fd598891000072307ee9ad801e0f25a87efc4052
Lorena Paixão <lorenamagalhaes4127@gmail.com>
2023-07-24 22:18:07 -0300

    exercicio 9

---
+++ exercicio-logica-variaveis/ex9.js
@@ -0,0 +1,9 @@
+let ladosPoligono = 6;
+const somaAngulos = (ladosPoligono - 2) * 180
+
+// calcule quanto vale cada um desses angulos, caso sejam todos iguais.
+
+const valorAngulo = somaAngulos / ladosPoligono;
+
+console.log(somaAngulos);
+console.log(valorAngulo);
