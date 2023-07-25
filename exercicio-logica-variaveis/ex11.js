9a92ce91ee0c9fbc8326af8b51ee222237d75f3c
Lorena Paixão <lorenamagalhaes4127@gmail.com>
2023-07-24 22:39:18 -0300

    exercicio 11

---
+++ exercicio-logica-variaveis/ex11.js
@@ -0,0 +1,5 @@
+let diametro = 6;
+let raio = diametro / 2;
+console.log(raio);
+const volumeDaEsfera = (4 * (raio ** 3) * Math.PI) / 3;
+console.log(`O volume de uma esfera de raio ${raio} é ${volumeDaEsfera}`);
